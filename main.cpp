aa0444c204a8be689276a4806f03ddc1cf49cbad
allis <allisonthompson1996@gmail.com>
2019-02-28 12:21:22 -0800

    pass id through thread

--- main.cpp
+++ main.cpp
@@ -6,21 +6,21 @@
 #include <chrono>
 #include <unistd.h>
 
-void Marco()
+void Marco(int id) //reorder loop to go around thread?
 {
 	for (int i=0; i<10; i++)
 	{
 		std::cout << "Marco\n";
-		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000)); //1 second
 	}
 }
 
-void Polo()
+void Polo(int id)
 {
 	for (int i=0; i<10; i++)
 	{
 		std::cout << "Polo\n";
-		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000)); //1 second
 	}
 }
 
@@ -29,10 +29,10 @@ void Polo()
 int main()
 {
     int n = 0;
-    std::thread t1(Marco); // t1 is not a thread
-    std::thread t2(Polo); // pass by value
+    std::thread t1(Marco, n);
+    std::thread t2(Polo, n);
 		t1.join();
 		t2.join();
 
-    std::cout << "We're done" << '\n';
+    std::cout << "That's all, folks";
 }
