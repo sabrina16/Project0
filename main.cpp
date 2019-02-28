00993bbad4c503c7337f980dad2bd0f0f775bae8
allis <allisonthompson1996@gmail.com>
2019-02-28 14:50:18 -0800

    ID added, Polo needs character return

--- main.cpp
+++ main.cpp
@@ -6,20 +6,20 @@
 #include <chrono>
 #include <unistd.h>
 
-void Marco(int id) //reorder loop to go around thread?
+void Marco() //each thread loops 10 times
 {
-	for (int i=0; i<10; i++)
+	for (int i=1; i<=10; i++)
 	{
-		std::cout << "Marco\n";
+		std::cout << "ID:" << i << " Marco\n";
 		std::this_thread::sleep_for(std::chrono::milliseconds(1000)); //1 second
 	}
 }
 
-void Polo(int id)
+void Polo() //add character return
 {
-	for (int i=0; i<10; i++)
+	for (int i=1; i<=10; i++)
 	{
-		std::cout << "Polo\n";
+		std::cout << "ID:" << i << " Polo\n";
 		std::this_thread::sleep_for(std::chrono::milliseconds(1000)); //1 second
 	}
 }
@@ -29,10 +29,11 @@ void Polo(int id)
 int main()
 {
     int n = 0;
-    std::thread t1(Marco, n);
-    std::thread t2(Polo, n);
+    std::thread t1(Marco);
+    std::thread t2(Polo);
 		t1.join();
 		t2.join();
 
-    std::cout << "That's all, folks";
+    std::cout << "That's all, folks\n";
+		return 0;
 }
